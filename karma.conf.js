cf154f74a793b79d8a68586957c76b28efbecf24
wenyumei <wenyumei@zhumengyuan.com>
2019-07-31 18:41:57 +0800

    update: #83152 配置文件

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/operation-web-test'),
+      dir: require('path').join(__dirname, './coverage/operation-web'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
