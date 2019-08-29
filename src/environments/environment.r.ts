5d37b374502b5d41cd7ca58c4403877c690b151c
yangsiyu0903 <758479868@qq.com>
2019-08-30 02:31:34 +0800

    fix: #86998 经济公司管理-地址也有过长现象， 建议鼠标放上也悬浮显示全部内容

--- src/environments/environment.r.ts
+++ src/environments/environment.r.ts
@@ -3,6 +3,6 @@ export const environment = {
   version: 'r',
   OPERATION_SERVE: '////operation-server.parkone.cn',
   STORAGE_DOMAIN: 'https://uustorage-t.uucin.com',
-  X_CLIENT_ID: '5bc90c26c7a011e996b10242ac1a0004',
-  X_CLIENT_MD5: 'e13ea48916a1584962d883573e14a371',
+  X_CLIENT_ID: '2f0ac90ac7a411e9b8270242ac160002',
+  X_CLIENT_MD5: '2ee90f194b122776ac4f57af2ae2e0e8',
 };
