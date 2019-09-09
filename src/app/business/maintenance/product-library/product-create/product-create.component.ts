bd0c7f8a3fc69cead69ec879d82e7559d63c2525
liyang <liyang@zhumengyuan.com>
2019-09-09 17:12:59 +0800

    update: #87755 产品库》添加、编辑产品

--- src/app/business/maintenance/product-library/product-create/product-create.component.ts
+++ src/app/business/maintenance/product-library/product-create/product-create.component.ts
@@ -1,5 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { isNullOrUndefined } from 'util';
+import { ZPhotoSelectComponent } from '../../../../share/components/z-photo-select/z-photo-select.component';
 
+
+export class ErrMessageItem {
+  public isError = false;
+  public errMes: string;
+
+  constructor(isError?: boolean, errMes?: string) {
+    if (!isError || isNullOrUndefined(errMes)) {
+      return;
+    }
+    this.isError = isError;
+    this.errMes = errMes;
+  }
+}
+export class ErrPositionItem {
+  icon: ErrMessageItem = new ErrMessageItem();
+  ic_name: ErrMessageItem = new ErrMessageItem();
+
+  constructor(icon?: ErrMessageItem, title?: ErrMessageItem, ic_name?: ErrMessageItem,
+    corner?: ErrMessageItem) {
+    if (isNullOrUndefined(icon) || isNullOrUndefined(ic_name)) {
+      return;
+    }
+    this.icon = icon;
+    this.ic_name = ic_name;
+  }
+}
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
@@ -11,9 +39,27 @@ export class ProductCreateComponent implements OnInit {
 
   public productErrMsg = ''; // 错误信息
 
+  public cover_url = []; // 图片集合
+
+  public errPositionItem: ErrPositionItem = new ErrPositionItem();
+
+  @ViewChild('coverImg', { static: false }) public coverImgSelectComponent: ZPhotoSelectComponent;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onSelectedPicture(event: any) {
+    this.errPositionItem.icon.isError = false;
+    if (event === 'type_error') {
+      this.errPositionItem.icon.isError = true;
+      this.errPositionItem.icon.errMes = '格式错误，请重新上传！';
+    } else if (event === 'size_over') {
+      this.errPositionItem.icon.isError = true;
+      this.errPositionItem.icon.errMes = '图片大小不得高于2M！';
+    }
   }
 
 }
