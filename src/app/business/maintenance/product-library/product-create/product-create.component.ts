d8cb2d8c3ee05df8acdda1baa7c8c503a7e76f3d
liyang <liyang@zhumengyuan.com>
2019-09-16 14:21:42 +0800

    update: #87755 产品库》添加、编辑产品

--- src/app/business/maintenance/product-library/product-create/product-create.component.ts
+++ src/app/business/maintenance/product-library/product-create/product-create.component.ts
@@ -7,6 +7,7 @@ import { ProductLibraryHttpService, ProductEntity } from '../product-library-htt
 import { ProjectDialogComponent } from '../../../../share/components/project-dialog/project-dialog.component';
 import { ProjectCategory } from '../../../../share/pipes/project-type.pipe';
 import { SelectBrandFirmComponent } from '../../../../share/components/select-brand-firm/select-brand-firm.component';
+import { HttpErrorEntity } from 'src/app/core/http.service';
 
 export class ErrMessageItem {
   public isError = false;
@@ -51,6 +52,8 @@ export class ProductCreateComponent implements OnInit {
 
   public productParams = []; // 产品 新建/编辑参数
 
+  public projectTypes = [1, 2]; // 项目类型 1:配件 2:服务
+
   public productErrMsg = ''; // 错误信息
 
   public cover_url = []; // 图片集合
@@ -65,7 +68,7 @@ export class ProductCreateComponent implements OnInit {
 
   public errPositionItem: ErrPositionItem = new ErrPositionItem();
 
-  @ViewChild('coverImg', { static: false }) public coverImgSelectComponent: ZPhotoSelectComponent;
+  @ViewChild('productImg', { static: false }) public productImgSelectComponent: ZPhotoSelectComponent;
   @ViewChild(ProjectDialogComponent, { static: true }) public projectDialogComponent: ProjectDialogComponent;
   @ViewChild(SelectBrandFirmComponent, { static: true }) public selectBrandFirmComponent: SelectBrandFirmComponent;
 
@@ -102,10 +105,38 @@ export class ProductCreateComponent implements OnInit {
   }
 
   private initForm() {
+    this.selected_brand_firm_info = this.productRecord.vehicle_brand ? (this.productRecord.vehicle_brand.vehicle_brand_name + ' > '
+      + this.productRecord.vehicle_firm.vehicle_firm_name) : null;
+    this.productRecord.upkeep_item_id = this.productRecord.upkeep_item.upkeep_item_id;
+    this.productRecord.upkeep_accessory_type = this.productRecord.upkeep_item.upkeep_item_type;
+    this.selected_project_info = this.productRecord.upkeep_item ?
+      (this.projectCategory[this.productRecord.upkeep_item.upkeep_item_category] + ' > '
+        + this.productRecord.upkeep_item.upkeep_item_name) : null;
+    this.cover_url = this.productRecord.image_url ? this.productRecord.image_url.split(',') : [];
+    this.productRecord.upkeep_item = null;
+    this.productRecord.vehicle_brand = null;
+    this.productRecord.vehicle_firm = null;
+    this.productRecord.created_time = null;
+    this.productRecord.updated_time = null;
+    this.productRecord.upkeep_merchants = null;
+    if (this.productRecord.upkeep_accessory_type === this.projectTypes[1]) {
+      this.productRecord.is_original = null;
+      this.productRecord.vehicle_brand_id = null;
+      this.productRecord.vehicle_firm_id = null;
+      this.productRecord.is_brand_special = null;
+      this.productRecord.serial_number = null;
+      this.productRecord.specification = null;
+      this.productRecord.number = null;
+    }
+  }
+
+  public onClearErrMsg() {
+    this.productErrMsg = '';
   }
 
   // 打开所属项目选择组件
   public onOpenProjectModal() {
+    this.onClearErrMsg();
     this.projectDialogComponent.open();
   }
 
@@ -113,14 +144,24 @@ export class ProductCreateComponent implements OnInit {
   public onSelectedProject(event: any) {
     if (event) {
       this.selectedProjectid = event.project.upkeep_item_id;
+      this.productRecord = new ProductEntity();
+      this.selected_brand_firm_info = null;
+      this.cover_url = [];
       this.productRecord.upkeep_item_id = event.project.upkeep_item_id;
+      this.productRecord.upkeep_accessory_type = event.project.upkeep_item_type;
       this.selectedCategory = event.category;
       this.selected_project_info = this.projectCategory[event.category] + ' > ' + event.project.upkeep_item_name;
     }
   }
 
+  // 变更是否原厂
+  public onChangeOriginal(event: any) {
+    this.productRecord.is_original = event.target.value === 'false' ? false : true;
+  }
+
   // 打开所属厂商选择组件
   public onOpenBrandFirmModal() {
+    this.onClearErrMsg();
     this.selectBrandFirmComponent.open();
   }
 
@@ -136,6 +177,7 @@ export class ProductCreateComponent implements OnInit {
   }
 
   public onSelectedPicture(event: any) {
+    this.onClearErrMsg();
     this.errPositionItem.icon.isError = false;
     if (event === 'type_error') {
       this.errPositionItem.icon.isError = true;
@@ -143,6 +185,139 @@ export class ProductCreateComponent implements OnInit {
     } else if (event === 'size_over') {
       this.errPositionItem.icon.isError = true;
       this.errPositionItem.icon.errMes = '图片大小不得高于2M！';
+    }
+  }
+
+  /** 金额 keyup 事件 */
+  public onMoneyKeyUp() {
+    debugger;
+    if (this.productRecord.original_amount) {
+      if (isNaN(parseFloat(String(this.productRecord.original_amount)))) {
+        this.productRecord.original_amount = null;
+      } else {
+        this.productRecord.original_amount = parseFloat(String(this.productRecord.original_amount));
+      }
+    }
+
+    if (this.productRecord.sale_amount) {
+      if (isNaN(parseFloat(String(this.productRecord.sale_amount)))) {
+        this.productRecord.sale_amount = null;
+      } else {
+        this.productRecord.sale_amount = parseFloat(String(this.productRecord.sale_amount));
+      }
+    }
+  }
+
+  /** 所需数量keyup事件 */
+  public onNumberKeyUp() {
+    if (this.productRecord.number) {
+      this.productRecord.number = Number(this.productRecord.number);
+      if (!this.productRecord.number) {
+        this.productRecord.number = 1;
+      }
+    }
+  }
+
+  // 键盘按下事件
+  public onKeydownEvent(event: any) {
+    if (event.keyCode === 13) {
+      this.onEditFormSubmit();
+      return false;
+    }
+  }
+
+  // 点击保存按钮
+  public onEditFormSubmit() {
+    this.onClearErrMsg();
+    this.productImgSelectComponent.upload().subscribe(() => {
+      const imageUrl = this.productImgSelectComponent.imageList.map(i => i.sourceUrl);
+      this.productRecord.image_url = imageUrl.join(',');
+      if (this.generateAndCheckParamsValid()) {
+        if (this.isCreateProduct) {
+          this.requestAddProduct();
+        } else {
+          this.requestUpdateProduct();
+        }
+      }
+    }, err => {
+      this.upLoadErrMsg(err);
+    });
+  }
+
+  // 添加
+  private requestAddProduct() {
+    this.productLibraryService.requestAddProductData(this.productRecord).subscribe(res => {
+      this.globalService.promptBox.open('保存成功', () => {
+        this.router.navigate(['../list'], { relativeTo: this.route });
+      });
+    }, err => {
+      this.errorProcess(err);
+    });
+  }
+
+  // 编辑
+  private requestUpdateProduct() {
+    this.productLibraryService.requestUpdateProductData(this.productRecord, this.product_id).subscribe(res => {
+      this.globalService.promptBox.open('保存成功', () => {
+        this.router.navigate(['../../list'], { relativeTo: this.route });
+      });
+    }, err => {
+      this.errorProcess(err);
+    });
+  }
+
+  /** 校验项目新增参数是否合法 */
+  private generateAndCheckParamsValid(): boolean {
+
+    if (!this.productRecord.upkeep_item_id || !this.selected_project_info) {
+      this.productErrMsg = '请选择所属项目！';
+      return false;
+    }
+
+    if (!this.productRecord.image_url) {
+      this.productErrMsg = '请上传图片！';
+      return false;
+    }
+
+    if (this.productRecord.original_amount > this.productRecord.sale_amount) {
+      this.productErrMsg = '销售单价应大于原价！';
+      return false;
+    }
+
+    if (this.productRecord.upkeep_accessory_type === this.projectTypes[0]) {
+      if (this.productRecord.is_original) {
+        if (!this.selected_brand_firm_info || !this.productRecord.vehicle_brand_id || !this.productRecord.vehicle_firm_id) {
+          this.productErrMsg = '请选择所属厂商！';
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  // 上传图片错误信息处理
+  private upLoadErrMsg(err: any) {
+    if (err.status === 422) {
+      this.globalService.promptBox.open('参数错误，可能文件格式错误！');
+    } else if (err.status === 413) {
+      this.globalService.promptBox.open('上传资源文件太大，服务器无法保存！');
+    } else {
+      this.globalService.httpErrorProcess(err);
+    }
+  }
+  // 接口错误信息处理
+  private errorProcess(err: any) {
+    if (!this.globalService.httpErrorProcess(err)) {
+      if (err.status === 422) {
+        const error: HttpErrorEntity = HttpErrorEntity.Create(err.error);
+        for (const content of error.errors) {
+          if (content.resource === 'upkeep_item_relation' && content.code === 'invalid') {
+            this.productErrMsg = '所选配套项目不存在！';
+            return;
+          }
+        }
+      }
     }
   }
 
