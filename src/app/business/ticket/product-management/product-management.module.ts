c8d8d708c3fb4f604db0b1024876e04dd1e7f8f5
liyang <liyang@zhumengyuan.com>
2019-10-08 09:20:57 +0800

    update: #90649 运营管理系统 》票务，路由搭建

--- src/app/business/ticket/product-management/product-management.module.ts
+++ src/app/business/ticket/product-management/product-management.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ShareModule } from '../../../share/share.module';
 import { ProductManagementRoutingModule } from './product-management-routing.module';
+import { ProductManagementComponent } from './product-management.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [
+    ProductManagementComponent,
+    ProductListComponent],
   imports: [
+    ShareModule,
     CommonModule,
     ProductManagementRoutingModule
   ]
--- src/app/business/ticket/ticket.module.ts
+++ src/app/business/ticket/ticket.module.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { TicketRoutingModule } from './ticket-routing.module';
 import { TicketComponent } from './ticket.component';
-import { ProductManagementComponent } from './product-management/product-management.component';
-
 
 @NgModule({
-  declarations: [TicketComponent, ProductManagementComponent],
+  declarations: [TicketComponent],
   imports: [
     CommonModule,
     TicketRoutingModule
