9c8e5842fca6ca3b99359fa9f39b8a03b43e280c
wenyumei <wenyumei@zhumengyuan.com>
2019-11-08 11:01:05 +0800

    update: #94211 视频选择组件

--- src/app/business/mall/goods-management/goods-create/goods-create.component.ts
+++ src/app/business/mall/goods-management/goods-create/goods-create.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-goods-create',
-  templateUrl: './goods-create.component.html',
-  styleUrls: ['./goods-create.component.css']
+    selector: 'app-goods-create',
+    templateUrl: './goods-create.component.html',
+    styleUrls: ['./goods-create.component.css']
 })
 export class GoodsCreateComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-
+    public ngOnInit() {
+    }
 }
