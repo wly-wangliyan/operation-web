1626b198591c1849243b23a5764b4fdd22c9d2db
wenyumei <wenyumei@zhumengyuan.com>
2019-11-11 13:59:35 +0800

    update: #94124 商城>产品列表>新建产品

--- src/app/business/mall/goods-management/goods-create/goods-create.component.ts
+++ src/app/business/mall/goods-management/goods-create/goods-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { GlobalService } from '../../../../core/global.service';
+import { GoodsManagementHttpService } from '../goods-management-http.service';
 
 @Component({
     selector: 'app-goods-create',
@@ -7,7 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoodsCreateComponent implements OnInit {
 
-    constructor() {
+    constructor(private routerInfo: ActivatedRoute,
+                private router: Router,
+                private globalService: GlobalService,
+                private goodsManagementHttpService: GoodsManagementHttpService) {
+        this.routerInfo.params.subscribe((params: Params) => {
+        });
     }
 
     public ngOnInit() {
