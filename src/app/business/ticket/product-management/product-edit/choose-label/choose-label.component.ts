309767e4ab69913227ed4ccf9b3657e1e08186d9
wenyumei <wenyumei@zhumengyuan.com>
2019-11-13 17:14:45 +0800

    fix: #94916 修改sentry中提示的错误

--- src/app/business/ticket/product-management/product-edit/choose-label/choose-label.component.ts
+++ src/app/business/ticket/product-management/product-edit/choose-label/choose-label.component.ts
@@ -53,7 +53,6 @@ export class ChooseLabelComponent implements OnInit {
 
   // 查询按钮
   public onSearchBtnClick() {
-    this.searchParams.name = this.searchParams.name && this.searchParams.name.trim();
     this.searchText$.next();
   }
 
