deb5538f754122e90be768b5f5bf27b0aa669aa5
wenyumei <wenyumei@zhumengyuan.com>
2019-11-19 14:43:12 +0800

    update: #95421 项目路由结构拆分

--- src/app/operational-system/operational-system-routing.module.ts
+++ src/app/operational-system/operational-system-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './core/auth-guard.service';
+import { AppComponent } from './app.component';
+import { ZPromptBoxComponent } from './share/components/tips/z-prompt-box/z-prompt-box.component';
+import { RouteMonitorService } from './core/route-monitor.service';
 
-
+const routes: Routes = [
+    { path: 'login', loadChildren: './business/login/login.module#LoginModule', canActivate: [AuthGuardService] },
+    {
+        path: '', component: AppComponent, children: [
+        { path: '', redirectTo: 'main', pathMatch: 'full' },
+        { path: 'main', loadChildren: './business/main/main.module#MainModule', canActivate: [AuthGuardService, RouteMonitorService] },
+        { path: '**', redirectTo: 'main', pathMatch: 'full' }
+    ]
+    }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class OperationalSystemRoutingModule { }
+export class OperationalSystemRoutingModule {
+}
