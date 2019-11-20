1fd2b2d0a16c1e11d1e3135d19b1b3bf8d84d2c7
wenyumei <wenyumei@zhumengyuan.com>
2019-11-20 09:57:57 +0800

    update: #95345 门店保养路由搭建

--- src/app/store-maintenance/store-maintenance.component.ts
+++ src/app/store-maintenance/store-maintenance.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, Renderer2, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs/index';
+import { DateFormatHelper } from '../../utils/date-format-helper';
+import { AuthService } from '../core/auth.service';
+import { GlobalService } from '../core/global.service';
+import { RouteMonitorService } from '../core/route-monitor.service';
+import { ZPromptBoxComponent } from '../share/components/tips/z-prompt-box/z-prompt-box.component';
+import { ZConfirmationBoxComponent } from '../share/components/tips/z-confirmation-box/z-confirmation-box.component';
+import { Http403TipComponent } from '../share/components/tips/http403-tip/http403-tip.component';
+import { Http500TipComponent } from '../share/components/tips/http500-tip/http500-tip.component';
+import { ExpandedMenuComponent } from '../share/components/expanded-menu/expanded-menu.component';
+import { IntervalService } from '../business/notice-center/interval.service';
 
 @Component({
-  selector: 'app-store-maintenance',
-  templateUrl: './store-maintenance.component.html',
-  styleUrls: ['./store-maintenance.component.css']
+    selector: 'app-store-maintenance',
+    templateUrl: './store-maintenance.component.html',
+    styleUrls: ['./store-maintenance.component.css']
 })
-export class StoreMaintenanceComponent implements OnInit {
+export class StoreMaintenanceComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+    public user: string;
 
-  ngOnInit() {
-  }
+    private routePathSubscription: Subscription;
 
+    @ViewChild('routerDiv', {static: false}) public routerDiv: ElementRef;
+
+    @ViewChild(ZPromptBoxComponent, {static: false}) private promptBox: ZPromptBoxComponent;
+
+    @ViewChild(ZConfirmationBoxComponent, {static: false}) private confirmationBox: ZConfirmationBoxComponent;
+
+    @ViewChild(Http403TipComponent, {static: false}) public global403Tip: Http403TipComponent;
+
+    @ViewChild(Http500TipComponent, {static: false}) public global500Tip: Http500TipComponent;
+
+    @ViewChild(ExpandedMenuComponent, {static: false}) private menuComponent: ExpandedMenuComponent;
+
+    constructor(public authService: AuthService,
+                public globalService: GlobalService,
+                private routeMonitorService: RouteMonitorService,
+                private renderer2: Renderer2,
+                private intervalService: IntervalService) {
+        DateFormatHelper.NowBlock = () => {
+            return new Date(globalService.timeStamp * 1000);
+        };
+    }
+
+    public ngAfterViewInit() {
+        this.globalService.promptBox = this.promptBox;
+        this.globalService.confirmationBox = this.confirmationBox;
+        this.globalService.http403Tip = this.global403Tip;
+        this.globalService.http500Tip = this.global500Tip;
+        GlobalService.Instance = this.globalService;
+        this.routeMonitorService.routePathChanged.subscribe(path => {
+            // 到路由变更时重置显示状态
+            this.global403Tip.http403Flag = false;
+            this.global500Tip.http500Flag = false;
+        });
+    }
+
+    public ngOnDestroy() {
+        this.routePathSubscription && this.routePathSubscription.unsubscribe();
+        this.intervalService.stopTimer();
+    }
+
+    // 退出
+    public onLoginoutClick() {
+        this.confirmationBox.open('提示', '确认退出?', () => {
+            this.confirmationBox.close();
+            this.authService.logout();
+        });
+    }
+
+    public displayStateChanged(): void {
+        if (this.global403Tip.http403Flag || this.global500Tip.http500Flag) {
+            this.renderer2.setStyle(this.routerDiv.nativeElement, 'display', 'none');
+        } else {
+            this.renderer2.setStyle(this.routerDiv.nativeElement, 'display', 'block');
+        }
+    }
 }
+
