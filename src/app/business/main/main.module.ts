310bc11aaab73932e18990e6a008e52270dcfd33
wenyumei <wenyumei@zhumengyuan.com>
2019-11-20 09:40:20 +0800

    update: #95421 项目路由结构拆分

--- src/app/business/main/main.module.ts
+++ src/app/business/main/main.module.ts
@@ -1,25 +1,26 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {MainRoutingModule} from './main-routing.module';
-import {MainComponent} from './main.component';
-import {NgZorroAntdModule} from 'ng-zorro-antd';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HomeComponent} from './home/home.component';
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MainRoutingModule,
-    NgZorroAntdModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ColorPickerModule
-  ],
-  declarations: [
-    MainComponent,
-    HomeComponent]
+    imports: [
+        CommonModule,
+        MainRoutingModule,
+        NgZorroAntdModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ColorPickerModule
+    ],
+    declarations: [
+        MainComponent,
+        HomeComponent
+    ]
 })
 export class MainModule {
 }
--- src/app/store-maintenance/store-maintenance-entry/store-maintenance-entry.component.ts
+++ src/app/store-maintenance/store-maintenance-entry/store-maintenance-entry.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { initializer } from '../../initializer';
+import { AuthService } from '../../core/auth.service';
+import { HttpService } from '../../core/http.service';
 
 @Component({
-  selector: 'app-store-maintenance-entry',
-  templateUrl: './store-maintenance-entry.component.html',
-  styleUrls: ['./store-maintenance-entry.component.css']
+    selector: 'app-store-maintenance-entry',
+    templateUrl: './store-maintenance-entry.component.html',
+    styleUrls: ['./store-maintenance-entry.component.css']
 })
-export class StoreMaintenanceEntryComponent implements OnInit {
+export class StoreMaintenanceEntryComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(private authService: AuthService, private httpService: HttpService) {
+        authService.authorizeBySecretKey(initializer.user);
+        httpService.setStartTimeStamp(initializer.startTimeStamp);
+    }
 }
