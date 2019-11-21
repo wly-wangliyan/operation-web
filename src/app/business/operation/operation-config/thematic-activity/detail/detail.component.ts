6685eeb8d1f5170a7ae259b0ba23b6f26bc602ef
liyang <liyang@zhumengyuan.com>
2019-11-21 14:50:10 +0800

    update: #95748 统计明细增加分页，需要对应接口

--- src/app/business/operation/operation-config/thematic-activity/detail/detail.component.ts
+++ src/app/business/operation/operation-config/thematic-activity/detail/detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalService } from '../../../../../core/global.service';
 import { ThematicActivityService, ThematicEntity, ReadStatisticsEntity } from '../thematic-activity.service';
+import { Subscription, forkJoin } from 'rxjs';
+
+const PageSize = 15;
 
 @Component({
   selector: 'app-detail',
@@ -14,7 +17,22 @@ export class DetailComponent implements OnInit {
 
   public readStatisticsList: Array<ReadStatisticsEntity> = []; // 阅读统计列表
 
+  public pageIndex = 1; // 当前页码
+
+  public noResultText = '数据加载中...';
+
+  private linkUrl: string; // 分页url
+
+  private continueRequestSubscription: Subscription;
+
   private activity_id: string; // 专题活动id
+
+  private get pageCount(): number {
+    if (this.readStatisticsList.length % PageSize === 0) {
+      return this.readStatisticsList.length / PageSize;
+    }
+    return this.readStatisticsList.length / PageSize + 1;
+  }
 
   constructor(
     private route: ActivatedRoute,
@@ -34,13 +52,33 @@ export class DetailComponent implements OnInit {
     }
   }
 
-  // 获取产品详情
+  // 获取产品详情及浏览量统计列表
   private getThematicDetail(): void {
-    this.thematicService.requestThematicDetail(this.activity_id).subscribe(data => {
-      this.thematicRecord = data;
-      this.readStatisticsList = data.click_stats ? data.click_stats : [];
-    }, err => {
-      this.globalService.httpErrorProcess(err);
-    });
+    forkJoin(this.thematicService.requestThematicDetail(this.activity_id),
+      this.thematicService.requestClickListData(this.activity_id)).subscribe(res => {
+        this.thematicRecord = res[0];
+        this.readStatisticsList = res[1].results;
+        this.linkUrl = res[1].linkUrl;
+        this.noResultText = '暂无数据';
+      }, err => {
+        this.noResultText = '暂无数据';
+        this.globalService.httpErrorProcess(err);
+      });
+  }
+
+  // 翻页方法
+  public onNZPageIndexChange(pageIndex: number): void {
+    this.pageIndex = pageIndex;
+    if (pageIndex + 1 >= this.pageCount && this.linkUrl) {
+      // 当存在linkUrl并且快到最后一页了请求数据
+      this.continueRequestSubscription && this.continueRequestSubscription.unsubscribe();
+      this.continueRequestSubscription = this.thematicService.continueClickListData(this.linkUrl)
+        .subscribe(res => {
+          this.readStatisticsList = this.readStatisticsList.concat(res.results);
+          this.linkUrl = res.linkUrl;
+        }, err => {
+          this.globalService.httpErrorProcess(err);
+        });
+    }
   }
 }
