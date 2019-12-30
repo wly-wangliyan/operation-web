493e747f9bb4049612b6337e44f5612957d1a523
wenyumei <wenyumei@zhumengyuan.com>
2019-12-30 15:04:54 +0800

    update: #100064 修改sentry中提示的错误

--- src/app/exemption/service-config/service-config.component.ts
+++ src/app/exemption/service-config/service-config.component.ts
@@ -5,148 +5,149 @@ import { Subscription, Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-service-config',
-  templateUrl: './service-config.component.html',
-  styleUrls: ['./service-config.component.css']
+    selector: 'app-service-config',
+    templateUrl: './service-config.component.html',
+    styleUrls: ['./service-config.component.css']
 })
 export class ServiceConfigComponent implements OnInit, OnDestroy {
 
-  public loading = true;
+    public loading = true;
 
-  public isEdit = false;
+    public isEdit = false;
 
-  public configParams: ConfigEntity = new ConfigEntity(); // 服务配置
+    public configParams: ConfigEntity = new ConfigEntity(); // 服务配置
 
-  public configErrMsg = ''; // 错误信息
+    public configErrMsg = ''; // 错误信息
 
-  private exemption_id = 'c0299b0b5fcc94d9051960f567c579a1'; // 配置id
+    private exemption_id = 'c0299b0b5fcc94d9051960f567c579a1'; // 配置id
 
-  private requestSubscription: Subscription;
+    private requestSubscription: Subscription;
 
-  private searchText$ = new Subject<any>();
+    private searchText$ = new Subject<any>();
 
-  private is_save = false; // 标记是否保存中
+    private is_save = false; // 标记是否保存中
 
-  private isInstanceReady = false; // 标记富文本编辑器是否加载完成;
+    private isInstanceReady = false; // 标记富文本编辑器是否加载完成;
 
-  constructor(private globalService: GlobalService, private serviceConfigService: ServiceConfigService) { }
-
-  public ngOnInit() {
-    this.is_save = false;
-    this.searchText$.pipe(debounceTime(500)).subscribe(() => {
-      this.rquestConfigDetail();
-    });
-    this.searchText$.next();
-  }
-
-  public ngOnDestroy() {
-    this.requestSubscription && this.requestSubscription.unsubscribe();
-    this.searchText$ && this.searchText$.unsubscribe();
-    CKEDITOR.instances.serviceConfigEditor.destroy(true);
-  }
-
-  // 获取服务配置详情
-  private rquestConfigDetail(): void {
-    this.requestSubscription && this.requestSubscription.unsubscribe();
-    this.requestSubscription = this.serviceConfigService.requestServiceConfigDetail(this.exemption_id).subscribe(res => {
-      this.configParams = new ConfigEntity(res);
-      this.getEditorData();
-      this.loading = false;
-    }, err => {
-      this.loading = false;
-      this.getEditorData();
-      this.globalService.httpErrorProcess(err);
-    });
-  }
-
-  // 富文本数据处理
-  public getEditorData() {
-    const tempContent = this.configParams.content.replace('/\r\n/g', '').replace(/\n/g, '');
-    CKEDITOR.instances.serviceConfigEditor.setData(tempContent);
-    CKEDITOR.instances.serviceConfigEditor.setReadOnly(true);
-    this.isInstanceReady = true;
-  }
-
-  // 富文本编辑器是否有值
-  public isAlreadyFill(): boolean {
-    if (this.isInstanceReady) {
-      return CKEDITOR.instances.serviceConfigEditor.getData() ? true : false;
-    } else {
-      return false;
+    constructor(private globalService: GlobalService, private serviceConfigService: ServiceConfigService) {
     }
-  }
 
-  public onEditFormSubmit(): void {
-
-    if (this.is_save) {
-      return;
-    }
-    if (this.generateAndCheckParamsValid()) {
-      this.is_save = true;
-      this.serviceConfigService.requestUpdateServiceConfigDetail(this.exemption_id, this.configParams).subscribe(() => {
-        this.globalService.promptBox.open('保存成功!');
+    public ngOnInit() {
         this.is_save = false;
-        this.isEdit = false;
+        this.searchText$.pipe(debounceTime(500)).subscribe(() => {
+            this.rquestConfigDetail();
+        });
         this.searchText$.next();
-      }, err => {
-        if (!this.globalService.httpErrorProcess(err)) {
-          this.globalService.promptBox.open('保存失败，请重试！', null, 2000, null, false);
-          this.is_save = false;
+    }
+
+    public ngOnDestroy() {
+        this.requestSubscription && this.requestSubscription.unsubscribe();
+        this.searchText$ && this.searchText$.unsubscribe();
+        CKEDITOR.instances.serviceConfigEditor.destroy(true);
+    }
+
+    // 获取服务配置详情
+    private rquestConfigDetail(): void {
+        this.requestSubscription && this.requestSubscription.unsubscribe();
+        this.requestSubscription = this.serviceConfigService.requestServiceConfigDetail(this.exemption_id).subscribe(res => {
+            this.configParams = new ConfigEntity(res);
+            this.getEditorData();
+            this.loading = false;
+        }, err => {
+            this.loading = false;
+            this.getEditorData();
+            this.globalService.httpErrorProcess(err);
+        });
+    }
+
+    // 富文本数据处理
+    public getEditorData() {
+        const tempContent = this.configParams.content.replace('/\r\n/g', '').replace(/\n/g, '');
+        CKEDITOR.instances.serviceConfigEditor.setData(tempContent);
+        CKEDITOR.instances.serviceConfigEditor.setReadOnly(true);
+        this.isInstanceReady = true;
+    }
+
+    // 富文本编辑器是否有值
+    public isAlreadyFill(): boolean {
+        if (this.isInstanceReady) {
+            return CKEDITOR.instances.serviceConfigEditor.getData() ? true : false;
+        } else {
+            return false;
         }
-      });
-    } else {
-      this.is_save = false;
-    }
-  }
-
-  private generateAndCheckParamsValid(): boolean {
-    const tempContent = CKEDITOR.instances.serviceConfigEditor.getData();
-
-    if (!this.configParams.total_amount || Number(this.configParams.total_amount) === 0) {
-      this.configErrMsg = '原价应大于0！';
-      return false;
     }
 
-    if (!this.configParams.sale_amount || Number(this.configParams.sale_amount) === 0) {
-      this.configErrMsg = '结算价应大于0！';
-      return false;
+    public onEditFormSubmit(): void {
+
+        if (this.is_save) {
+            return;
+        }
+        if (this.generateAndCheckParamsValid()) {
+            this.is_save = true;
+            this.serviceConfigService.requestUpdateServiceConfigDetail(this.exemption_id, this.configParams).subscribe(() => {
+                this.globalService.promptBox.open('保存成功!');
+                this.is_save = false;
+                this.isEdit = false;
+                this.searchText$.next();
+            }, err => {
+                if (!this.globalService.httpErrorProcess(err)) {
+                    this.globalService.promptBox.open('保存失败，请重试！', null, 2000, null, false);
+                    this.is_save = false;
+                }
+            });
+        } else {
+            this.is_save = false;
+        }
     }
 
-    if (!this.configParams.real_amount || Number(this.configParams.real_amount) === 0) {
-      this.configErrMsg = '售价应大于0！';
-      return false;
+    private generateAndCheckParamsValid(): boolean {
+        const tempContent = CKEDITOR.instances.serviceConfigEditor.getData();
+
+        if (!this.configParams.total_amount || Number(this.configParams.total_amount) === 0) {
+            this.configErrMsg = '原价应大于0！';
+            return false;
+        }
+
+        if (!this.configParams.sale_amount || Number(this.configParams.sale_amount) === 0) {
+            this.configErrMsg = '结算价应大于0！';
+            return false;
+        }
+
+        if (!this.configParams.real_amount || Number(this.configParams.real_amount) === 0) {
+            this.configErrMsg = '售价应大于0！';
+            return false;
+        }
+
+        if (Number(this.configParams.total_amount) < Number(this.configParams.real_amount)) {
+            this.configErrMsg = '原价应大于等于售价！';
+            return false;
+        }
+
+        if (Number(this.configParams.real_amount) < Number(this.configParams.sale_amount)) {
+            this.configErrMsg = '结算价应小于等于售价！';
+            return false;
+        }
+
+        if (!tempContent) {
+            this.configErrMsg = '请填写图文详情！';
+            return false;
+        } else {
+            this.configParams.content = tempContent.replace('/\r\n/g', '').replace(/\n/g, '');
+        }
+        return true;
     }
 
-    if (Number(this.configParams.total_amount) < Number(this.configParams.real_amount)) {
-      this.configErrMsg = '原价应大于等于售价！';
-      return false;
+    // 启用编辑
+    public onEditClick(): void {
+        this.isEdit = true;
+        CKEDITOR.instances.serviceConfigEditor.setReadOnly(false);
     }
 
-    if (Number(this.configParams.real_amount) < Number(this.configParams.sale_amount)) {
-      this.configErrMsg = '结算价应小于等于售价！';
-      return false;
+    // 取消编辑
+    public onCancelClick(): void {
+        this.isEdit = false;
+        this.configErrMsg = '';
+        this.searchText$.next();
+        CKEDITOR.instances.serviceConfigEditor.setReadOnly(true);
     }
-
-    if (!tempContent) {
-      this.configErrMsg = '请填写图文详情！';
-      return false;
-    } else {
-      this.configParams.content = tempContent.replace('/\r\n/g', '').replace(/\n/g, '');
-    }
-    return true;
-  }
-
-  // 启用编辑
-  public onEditClick(): void {
-    this.isEdit = true;
-    CKEDITOR.instances.serviceConfigEditor.setReadOnly(false);
-  }
-
-  // 取消编辑
-  public onCancelClick(): void {
-    this.isEdit = false;
-    this.configErrMsg = '';
-    this.searchText$.next();
-    CKEDITOR.instances.serviceConfigEditor.setReadOnly(true);
-  }
 }
