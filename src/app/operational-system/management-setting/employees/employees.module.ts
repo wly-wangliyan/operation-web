000409c9f845bdccb6a3ec7940d9e1502eb7b067
liyang <liyang@zhumengyuan.com>
2020-01-03 18:30:30 +0800

    update: #100594 优化ng-zerro组件库-management-setting

--- src/app/operational-system/management-setting/employees/employees.module.ts
+++ src/app/operational-system/management-setting/employees/employees.module.ts
@@ -4,13 +4,16 @@ import { ShareModule } from '../../../share/share.module';
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeesComponent } from './employees.component';
-
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @NgModule({
   declarations: [EmployeesComponent, EmployeeListComponent],
   imports: [
     ShareModule,
     CommonModule,
-    EmployeesRoutingModule
+    EmployeesRoutingModule,
+    NzTableModule,
+    NzButtonModule,
   ]
 })
 export class EmployeesModule { }
