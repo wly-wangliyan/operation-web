3ffcd697ff07b3f65b505b584afa278478b3bf23
wenyumei <wenyumei@zhumengyuan.com>
2020-01-06 15:12:09 +0800

    update: #100559 服务配置，费用分为室内室外，新增标志位名称

--- src/app/order-parking/service-config/service-config-edit/service-config-edit.component.ts
+++ src/app/order-parking/service-config/service-config-edit/service-config-edit.component.ts
@@ -1,202 +1,253 @@
-
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
-import { GlobalService } from '../../../core/global.service';
 import { Subject, timer } from 'rxjs/index';
 import { debounceTime } from 'rxjs/internal/operators';
 import { HttpErrorEntity } from '../../../core/http.service';
-import { ServiceConfigService, ParkingEntity, ServiceConfigParams } from '../service-config.service';
+import { GlobalService } from '../../../core/global.service';
+import { ServiceConfigService, ParkingEntity } from '../service-config.service';
 
 
 @Component({
-  selector: 'app-service-config-edit',
-  templateUrl: './service-config-edit.component.html',
-  styleUrls: ['./service-config-edit.component.css']
+    selector: 'app-service-config-edit',
+    templateUrl: './service-config-edit.component.html',
+    styleUrls: ['./service-config-edit.component.css']
 })
 export class ServiceConfigEditComponent implements OnInit {
 
-  constructor(private globalService: GlobalService, private serviceConfigService: ServiceConfigService,
-              private routerInfo: ActivatedRoute, private router: Router) {
-  }
+    public loading = false;
 
-  public parkingDetailData: ParkingEntity = new ParkingEntity();
-  public configParams: ServiceConfigParams = new ServiceConfigParams();
-  public checkLabelNamesList: Array<any> = [];
-  public tag = '';
-  public loading = false;
-  public parking_id: string;
-  public origin_fee = '';
-  public sale_fee = '';
-  public pre_fee = '';
-  public minus_fee = '';
-  public tagNameErrors = '';
-  public originPriceErrors = '';
-  public salePriceErrors = '';
-  public prePriceErrors = '';
-  public minusPriceErrors = '';
-  public minDaysErrors = '';
-  public mainTelErrors = '';
-  public standbyTelErrors = '';
-  public instructionErrors = '';
-  public noticeErrors = '';
+    public tag = '';
 
-  private searchText$ = new Subject<any>();
+    public parkingDetailData: ParkingEntity = new ParkingEntity();
 
-  ngOnInit() {
-    this.routerInfo.params.subscribe((params: Params) => {
-      this.parking_id = params.parking_id;
-    });
+    public sportTypeStatus: SportTypeStatusItem = new SportTypeStatusItem();
 
-    this.searchText$.pipe(debounceTime(500)).subscribe(() => {
-      this.serviceConfigService.requestParkingDetailData(this.parking_id).subscribe(res => {
-        this.parkingDetailData = res;
-        this.getDetailData();
-        this.getEditorData(this.parkingDetailData.instruction, this.parkingDetailData.notice);
-      }, err => {
-        this.globalService.httpErrorProcess(err);
-      });
-    });
-    this.searchText$.next();
-  }
+    public configInfoErrMsg: ConfigInfoErrorMsgItem = new ConfigInfoErrorMsgItem();
 
-  // 配置详情数据处理
-  private getDetailData() {
-    this.checkLabelNamesList = this.parkingDetailData.tags ? this.parkingDetailData.tags : [];
-    this.origin_fee = this.getFeeData(this.parkingDetailData.origin_fee);
-    this.sale_fee = this.getFeeData(this.parkingDetailData.sale_fee);
-    this.pre_fee = this.getFeeData(this.parkingDetailData.pre_fee);
-    this.minus_fee = this.getFeeData(this.parkingDetailData.minus_fee);
-    this.configParams.min_days = this.parkingDetailData.min_days;
-    this.configParams.main_tel = this.parkingDetailData.main_tel;
-    this.configParams.standby_tel = this.parkingDetailData.standby_tel;
-  }
+    private searchText$ = new Subject<any>();
 
-  // 价钱数据处理
-  private getFeeData(fee: number) {
-    return (fee || fee === 0) ? (Number(fee) / 100).toFixed(2) : '';
-  }
+    private parking_id: string;
 
-  // 富文本数据处理
-  private getEditorData(instruction: string, notice: string) {
-    CKEDITOR.instances.orderInstructionEditor.destroy(true);
-    CKEDITOR.instances.purchaseInstructionEditor.destroy(true);
-    CKEDITOR.replace('orderInstructionEditor').setData(instruction);
-    CKEDITOR.replace('purchaseInstructionEditor').setData(notice);
-  }
-
-
-  // 添加标签
-  public onAddTagClick() {
-    const labelList = this.checkLabelNamesList.filter(i => i.name === this.tag);
-    if (this.tag === '' || this.tag === null) {
-      this.tagNameErrors = '请勿添加空标签!';
-    } else if (labelList.length !== 0) {
-      this.tagNameErrors = '添加的标签重复!';
-    } else {
-      this.checkLabelNamesList.push(this.tag);
-      this.tag = '';
-      this.tagNameErrors = '';
-    }
-  }
-
-  // 删除标签
-  public onDelTag(i: number) {
-    this.checkLabelNamesList.splice(i, 1);
-  }
-
-  // 保存数据
-  public onSaveFormSubmit() {
-    const regPhone = /^(1[3-9])\d{9}$/g;
-    const regTel = /^\d{3}-\d{8}|(1[3-9])\d{9}$/g;
-    if (Number(this.sale_fee) > Number(this.origin_fee)) {
-      this.clear();
-      this.salePriceErrors = '售价不得大于原价！';
-    } else if (Number(this.pre_fee) > Number(this.sale_fee)) {
-      this.clear();
-      this.prePriceErrors = '预付费用不得大于售价！';
-    } else if (Number(this.minus_fee) > (Number(this.pre_fee))) {
-      this.clear();
-      this.minusPriceErrors = '下单立减不得大于预付费用！';
-    } else if (!regPhone.test(this.configParams.main_tel)) {
-      this.clear();
-      this.mainTelErrors = '请输入正确的常用电话！';
-    } else if (!regTel.test(this.configParams.standby_tel)) {
-      this.clear();
-      this.standbyTelErrors = '请输入正确的备用电话！';
-    } else if (this.configParams.main_tel === this.configParams.standby_tel) {
-      this.clear();
-      this.standbyTelErrors = '备用电话与常用电话重复！';
-    } else if (!CKEDITOR.instances.orderInstructionEditor.getData()) {
-      this.clear();
-      this.instructionErrors = '请输入预约说明！';
-    } else if (!CKEDITOR.instances.purchaseInstructionEditor.getData()) {
-      this.clear();
-      this.noticeErrors = '请输入购买须知！';
-    } else {
-      this.clear();
-      this.handleParams();
-      this.serviceConfigService.requestUpdateSweviceConfigData(this.configParams, this.parking_id).subscribe(() => {
-        this.globalService.promptBox.open('服务配置保存成功！');
-        this.searchText$.next();
-        timer(2000).subscribe(() => this.router.navigateByUrl('/order-parking/service-config'));
-      }, err => {
-        this.handleErrorFunc(err);
-      });
-    }
-  }
-
-  // 清空
-  private clear() {
-    this.tagNameErrors = '';
-    this.originPriceErrors = '';
-    this.salePriceErrors = '';
-    this.prePriceErrors = '';
-    this.minusPriceErrors = '';
-    this.minDaysErrors = '';
-    this.mainTelErrors = '';
-    this.standbyTelErrors = '';
-    this.instructionErrors = '';
-    this.noticeErrors = '';
-  }
-
-  // 处理服务配置入参
-  private handleParams() {
-    this.configParams.tags = this.checkLabelNamesList;
-    this.configParams.origin_fee = Number(this.origin_fee) * 100;
-    this.configParams.sale_fee = Number(this.sale_fee) * 100;
-    this.configParams.pre_fee = Number(this.pre_fee) * 100;
-    this.configParams.minus_fee = Number(this.minus_fee) * 100;
-    this.configParams.instruction = CKEDITOR.instances.orderInstructionEditor.getData().replace('/\r\n/g', '').replace(/\n/g, '');
-    this.configParams.notice = CKEDITOR.instances.purchaseInstructionEditor.getData().replace('/\r\n/g', '').replace(/\n/g, '');
-  }
-
-  // 处理错误信息
-  private handleErrorFunc(err) {
-    if (!this.globalService.httpErrorProcess(err)) {
-      if (err.status === 422) {
-        const error: HttpErrorEntity = HttpErrorEntity.Create(err.error);
-        for (const content of error.errors) {
-          const field = content.field === 'tags' ? '标签' : content.field === 'origin_fee' ?
-            '原价/日' : content.field === 'sale_fee' ? '售价/日' : content.field === 'pre_fee' ? '预付'
-              : content.field === 'minus_fee' ? '下单立减' : content.field === 'main_tel' ? '常用联系电话'
-                : content.field === 'standby_tel' ? '备用联系电话' : content.field === 'instruction' ? '预约说明'
-                  : content.field === 'notice' ? '购买须知' : '';
-          if (content.code === 'missing_field') {
-            this.globalService.promptBox.open(`${field}字段未填写!`, null, 2000, '/assets/images/warning.png');
-            return;
-          } else if (content.code === 'invalid') {
-            this.globalService.promptBox.open(`${field}字段输入错误`, null, 2000, '/assets/images/warning.png');
-          } else {
-            this.globalService.promptBox.open('服务配置保存失败,请重试!', null, 2000, '/assets/images/warning.png');
-          }
+    // 校验费用类型是否有效
+    public get CheckSportTypeValid(): boolean {
+        for (let sportTypeStatusIndex in this.sportTypeStatus) {
+            if (this.sportTypeStatus[sportTypeStatusIndex]) {
+                return true;
+            }
         }
-      }
+        return false;
     }
-  }
 
-  public onCancelBtn() {
-    this.router.navigateByUrl('/order-parking/service-config');
-  }
+    constructor(private globalService: GlobalService, private serviceConfigService: ServiceConfigService,
+                private routerInfo: ActivatedRoute, private router: Router) {
+        this.routerInfo.params.subscribe((params: Params) => {
+            this.parking_id = params.parking_id;
+        });
+    }
 
+    public ngOnInit() {
+        this.searchText$.pipe(debounceTime(500)).subscribe(() => {
+            this.serviceConfigService.requestParkingDetailData(this.parking_id).subscribe(res => {
+                this.parkingDetailData = new ParkingEntity(res);
+                this.sportTypeStatus = new SportTypeStatusItem(this.parkingDetailData.spot_types);
+                this.getEditorData(this.parkingDetailData.instruction, this.parkingDetailData.notice);
+            }, err => {
+                this.globalService.httpErrorProcess(err);
+            });
+        });
+        this.searchText$.next();
+    }
 
+    // 添加标签
+    public onAddTagClick() {
+        const labelList = this.parkingDetailData.tags.filter(i => i.name === this.tag);
+        if (this.tag === '' || this.tag === null) {
+            this.configInfoErrMsg.tagNameErrors = '请勿添加空标签!';
+        } else if (labelList.length !== 0) {
+            this.configInfoErrMsg.tagNameErrors = '添加的标签重复!';
+        } else {
+            this.parkingDetailData.tags.push(this.tag);
+            this.tag = '';
+            this.configInfoErrMsg.tagNameErrors = '';
+        }
+    }
+
+    // 删除标签
+    public onDelTag(i: number) {
+        this.parkingDetailData.tags.splice(i, 1);
+    }
+
+    // 保存数据
+    public onSaveFormSubmit() {
+        const regPhone = /^(1[3-9])\d{9}$/g;
+        const regTel = /^\d{3}-\d{8}|(1[3-9])\d{9}$/g;
+        const formatValueType = (sourceValue: any): number => {
+            return Number(sourceValue);
+        };
+        this.parkingDetailData.spot_types = [];
+        this.configInfoErrMsg = new ConfigInfoErrorMsgItem();
+
+        if (this.sportTypeStatus.isIndoorChecked) {
+            if (formatValueType(this.parkingDetailData.indoor_sale_fee) > formatValueType(this.parkingDetailData.indoor_origin_fee)) {
+                this.configInfoErrMsg.indoorSalePriceErrors = '售价不得大于原价！';
+                return;
+            }
+            if (formatValueType(this.parkingDetailData.indoor_pre_fee) > formatValueType(this.parkingDetailData.indoor_sale_fee)) {
+                this.configInfoErrMsg.indoorPrePriceErrors = '预付费用不得大于售价！';
+                return;
+            }
+            if (formatValueType(this.parkingDetailData.indoor_minus_fee) > formatValueType(this.parkingDetailData.indoor_pre_fee)) {
+                this.configInfoErrMsg.indoorMinusPriceErrors = '下单立减不得大于预付费用！';
+                return;
+            }
+            this.parkingDetailData.spot_types.push('1');
+        }
+        if (this.sportTypeStatus.isOutdoorChecked) {
+            if (formatValueType(this.parkingDetailData.sale_fee) > formatValueType(this.parkingDetailData.origin_fee)) {
+                this.configInfoErrMsg.salePriceErrors = '售价不得大于原价！';
+                return;
+            }
+            if (formatValueType(this.parkingDetailData.pre_fee) > formatValueType(this.parkingDetailData.sale_fee)) {
+                this.configInfoErrMsg.prePriceErrors = '预付费用不得大于售价！';
+                return;
+            }
+            if (formatValueType(this.parkingDetailData.minus_fee) > formatValueType(this.parkingDetailData.pre_fee)) {
+                this.configInfoErrMsg.minusPriceErrors = '下单立减不得大于预付费用！';
+                return;
+            }
+            this.parkingDetailData.spot_types.push('2');
+        }
+        if (!regPhone.test(this.parkingDetailData.main_tel)) {
+            this.configInfoErrMsg.mainTelErrors = '请输入正确的常用电话！';
+            return;
+        }
+        if (!regTel.test(this.parkingDetailData.standby_tel)) {
+            this.configInfoErrMsg.standbyTelErrors = '请输入正确的备用电话！';
+            return;
+        }
+        if (this.parkingDetailData.main_tel === this.parkingDetailData.standby_tel) {
+            this.configInfoErrMsg.standbyTelErrors = '备用电话与常用电话重复！';
+            return;
+        }
+        if (!CKEDITOR.instances.orderInstructionEditor.getData()) {
+            this.configInfoErrMsg.instructionErrors = '请输入预约说明！';
+            return;
+        }
+        if (!CKEDITOR.instances.purchaseInstructionEditor.getData()) {
+            this.configInfoErrMsg.noticeErrors = '请输入购买须知！';
+            return;
+        }
+
+        this.parkingDetailData.instruction = CKEDITOR.instances.orderInstructionEditor.getData().replace('/\r\n/g', '').replace(/\n/g, '');
+        this.parkingDetailData.notice = CKEDITOR.instances.purchaseInstructionEditor.getData().replace('/\r\n/g', '').replace(/\n/g, '');
+
+        this.serviceConfigService.requestUpdateServiceConfigData(this.parkingDetailData, this.parking_id).subscribe(() => {
+            this.globalService.promptBox.open('服务配置保存成功！');
+            this.searchText$.next();
+            timer(2000).subscribe(() => this.router.navigateByUrl('/order-parking/service-config'));
+        }, err => {
+            this.handleErrorFunc(err);
+        });
+    }
+
+    // 点击取消按钮取消编辑
+    public onCancelBtn() {
+        this.router.navigateByUrl('/order-parking/service-config');
+    }
+
+    // 富文本数据处理
+    private getEditorData(instruction: string, notice: string) {
+        CKEDITOR.instances.orderInstructionEditor.destroy(true);
+        CKEDITOR.instances.purchaseInstructionEditor.destroy(true);
+        CKEDITOR.replace('orderInstructionEditor', {width: 1200}).setData(instruction);
+        CKEDITOR.replace('purchaseInstructionEditor', {width: 1200}).setData(notice);
+    }
+
+    // 处理错误信息
+    private handleErrorFunc(err) {
+        if (!this.globalService.httpErrorProcess(err)) {
+            if (err.status === 422) {
+                const error: HttpErrorEntity = HttpErrorEntity.Create(err.error);
+                for (const content of error.errors) {
+                    let field = '';
+                    switch (content.field) {
+                        case 'tags':
+                            field = '标签';
+                            break;
+                        case 'origin_fee':
+                        case 'indoor_origin_fee':
+                            field = '原价/日';
+                            break;
+                        case 'sale_fee':
+                        case 'indoor_sale_fee':
+                            field = '售价/日';
+                            break;
+                        case 'pre_fee':
+                        case 'indoor_pre_fee':
+                            field = '预付';
+                            break;
+                        case 'minus_fee':
+                        case 'indoor_minus_fee':
+                            field = '下单立减';
+                            break;
+                        case 'main_tel':
+                            field = '常用联系电话';
+                            break;
+                        case 'standby_tel':
+                            field = '备用联系电话';
+                            break;
+                        case 'instruction':
+                            field = '预约说明';
+                            break;
+                        case 'notice':
+                            field = '购买须知';
+                            break;
+                    }
+                    if (content.code === 'missing_field') {
+                        this.globalService.promptBox.open(`${field}字段未填写!`, null, 2000, '/assets/images/warning.png');
+                        return;
+                    } else if (content.code === 'invalid') {
+                        this.globalService.promptBox.open(`${field}字段输入错误`, null, 2000, '/assets/images/warning.png');
+                    } else {
+                        this.globalService.promptBox.open('服务配置保存失败,请重试!', null, 2000, '/assets/images/warning.png');
+                    }
+                }
+            }
+        }
+    }
+}
+
+// 费用类别
+export class SportTypeStatusItem {
+    public isIndoorChecked: boolean = false;
+    public isOutdoorChecked: boolean = false;
+
+    constructor(sportType: Array<string> = []) {
+        sportType.forEach(sportTypeItem => {
+            // '1':室内车位 '2':室外车位
+            if (sportTypeItem === '1') {
+                this.isIndoorChecked = true;
+            } else if (sportTypeItem === '2') {
+                this.isOutdoorChecked = true;
+            }
+        });
+    }
+}
+
+// 错误提示消息
+export class ConfigInfoErrorMsgItem {
+    public tagNameErrors: string = ''; // 标签错误消息
+    public originPriceErrors: string = ''; // 室外原价错误消息
+    public salePriceErrors: string = ''; // 室外售价错误消息
+    public prePriceErrors: string = ''; // 室外预付费错误消息
+    public minusPriceErrors: string = ''; // 室外下单立减错误消息
+    public indoorOriginPriceErrors: string = ''; // 室内原价错误消息
+    public indoorSalePriceErrors: string = ''; // 室内售价错误消息
+    public indoorPrePriceErrors: string = ''; // 室内预付费错误消息
+    public indoorMinusPriceErrors: string = ''; // 室内下单立减错误消息
+    public minDaysErrors: string = ''; // 最低预付错误消息
+    public mainTelErrors: string = ''; // 常用电话错误消息
+    public standbyTelErrors: string = ''; // 备用电话误消息
+    public instructionErrors: string = ''; // 预约说明错误消息
+    public noticeErrors: string = ''; // 购买须知错误消息
 }
--- src/app/order-parking/service-config/service-config-list/service-config-list.component.ts
+++ src/app/order-parking/service-config/service-config-list/service-config-list.component.ts
@@ -9,92 +9,99 @@ import { AddCarParkingComponent } from './add-car-parking/add-car-parking.compon
 const PageSize = 15;
 
 @Component({
-  selector: 'app-service-config-list',
-  templateUrl: './service-config-list.component.html',
-  styleUrls: ['./service-config-list.component.css']
+    selector: 'app-service-config-list',
+    templateUrl: './service-config-list.component.html',
+    styleUrls: ['./service-config-list.component.css']
 })
 export class ServiceConfigListComponent implements OnInit {
-  public searchParams: ParkingSearchParams = new ParkingSearchParams(); // 条件筛选
-  public productConfigList: Array<ParkingEntity> = []; // 产品订单列表
 
-  public pageIndex = 1; // 当前页码
-  public noResultText = '数据加载中...';
+    public searchParams: ParkingSearchParams = new ParkingSearchParams(); // 条件筛选
 
-  public imageUrls = []; // 图片放大集合
+    public productConfigList: Array<ParkingEntity> = []; // 产品订单列表
 
-  private requestSubscription: Subscription; // 获取数据
-  private searchText$ = new Subject<any>();
-  private continueRequestSubscription: Subscription; // 分页获取数据
-  private linkUrl: string; // 分页url
+    public pageIndex = 1; // 当前页码
 
-  private get pageCount(): number {
-    if (this.productConfigList.length % PageSize === 0) {
-      return this.productConfigList.length / PageSize;
+    public noResultText = '数据加载中...';
+
+    public imageUrls = []; // 图片放大集合
+
+    private linkUrl: string; // 分页url
+
+    private searchText$ = new Subject<any>();
+
+    private requestSubscription: Subscription; // 获取数据
+
+    private continueRequestSubscription: Subscription; // 分页获取数据
+
+    private get pageCount(): number {
+        if (this.productConfigList.length % PageSize === 0) {
+            return this.productConfigList.length / PageSize;
+        }
+        return this.productConfigList.length / PageSize + 1;
     }
-    return this.productConfigList.length / PageSize + 1;
-  }
 
-  @ViewChild('addCarParking', { static: true }) public addCarParking: AddCarParkingComponent;
-  @ViewChild(ZPhotoSelectComponent, { static: true }) public ZPhotoSelectComponent: ZPhotoSelectComponent;
+    @ViewChild('addCarParking', {static: true}) public addCarParking: AddCarParkingComponent;
 
-  constructor(
-    private globalService: GlobalService,
-    private serviceConfigService: ServiceConfigService) { }
+    @ViewChild(ZPhotoSelectComponent, {static: true}) public ZPhotoSelectComponent: ZPhotoSelectComponent;
 
-  public ngOnInit() {
-    this.searchText$.pipe(debounceTime(500)).subscribe(() => {
-      this.serviceConfigService.requestParkingListData(this.searchParams).subscribe(res => {
-        this.productConfigList = res.results;
-        this.linkUrl = res.linkUrl;
-        this.pageIndex = 1;
-        this.noResultText = '暂无数据';
-      }, err => {
-        this.pageIndex = 1;
-        this.noResultText = '暂无数据';
-        this.globalService.httpErrorProcess(err);
-      });
-    });
-    this.searchText$.next();
-  }
+    constructor(private globalService: GlobalService,
+                private serviceConfigService: ServiceConfigService) {
+    }
 
-  // 添加停车场
-  public onAddCarParking() {
-    this.addCarParking.open(() => {
-      timer(1000).subscribe(() => {
+    public ngOnInit() {
+        this.searchText$.pipe(debounceTime(500)).subscribe(() => {
+            this.serviceConfigService.requestParkingListData(this.searchParams).subscribe(res => {
+                this.productConfigList = res.results;
+                this.linkUrl = res.linkUrl;
+                this.pageIndex = 1;
+                this.noResultText = '暂无数据';
+            }, err => {
+                this.pageIndex = 1;
+                this.noResultText = '暂无数据';
+                this.globalService.httpErrorProcess(err);
+            });
+        });
         this.searchText$.next();
-      });
-    });
-  }
+    }
 
-  /** 删除产品配置 */
-  public onDeleteProduct(data: ParkingEntity) {
-    this.globalService.confirmationBox.open('提示', '此操作不可逆，是否确认删除？', () => {
-      this.globalService.confirmationBox.close();
-      this.serviceConfigService.requestDeleteProductData(data.parking_id).subscribe(() => {
-        this.globalService.promptBox.open('删除成功', () => {
-          this.searchText$.next();
-        });
-      }, err => {
-        this.globalService.httpErrorProcess(err);
-      });
-    });
-  }
-
-
-  // 翻页
-  public onNZPageIndexChange(pageIndex: number) {
-    this.pageIndex = pageIndex;
-    if (pageIndex + 1 >= this.pageCount && this.linkUrl) {
-      // 当存在linkUrl并且快到最后一页了请求数据
-      this.continueRequestSubscription && this.continueRequestSubscription.unsubscribe();
-      this.continueRequestSubscription = this.serviceConfigService.continueParkingListData(this.linkUrl)
-        .subscribe(res => {
-          this.productConfigList = this.productConfigList.concat(res.results);
-          this.linkUrl = res.linkUrl;
-        }, err => {
-          this.globalService.httpErrorProcess(err);
+    // 添加停车场
+    public onAddCarParking() {
+        this.addCarParking.open(() => {
+            timer(1000).subscribe(() => {
+                this.searchText$.next();
+            });
         });
     }
-  }
+
+    /** 删除产品配置 */
+    public onDeleteProduct(data: ParkingEntity) {
+        this.globalService.confirmationBox.open('提示', '此操作不可逆，是否确认删除？', () => {
+            this.globalService.confirmationBox.close();
+            this.serviceConfigService.requestDeleteProductData(data.parking_id).subscribe(() => {
+                this.globalService.promptBox.open('删除成功', () => {
+                    this.searchText$.next();
+                });
+            }, err => {
+                this.globalService.httpErrorProcess(err);
+            });
+        });
+    }
+
+
+    // 翻页
+    public onNZPageIndexChange(pageIndex: number) {
+        this.pageIndex = pageIndex;
+        if (pageIndex + 1 >= this.pageCount && this.linkUrl) {
+            // 当存在linkUrl并且快到最后一页了请求数据
+            this.continueRequestSubscription && this.continueRequestSubscription.unsubscribe();
+            this.continueRequestSubscription = this.serviceConfigService.continueParkingListData(this.linkUrl)
+                .subscribe(res => {
+                    this.productConfigList = this.productConfigList.concat(res.results);
+                    this.linkUrl = res.linkUrl;
+                }, err => {
+                    this.globalService.httpErrorProcess(err);
+                });
+        }
+    }
 }
 
