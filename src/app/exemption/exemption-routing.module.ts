e102094d7632376a30d68593a74b4e18da088230
guodongxu <guodongxu@zhumengyuan.com>
2020-01-07 14:25:01 +0800

    update: #100816 路由优化--免检换帖

--- src/app/exemption/exemption-routing.module.ts
+++ src/app/exemption/exemption-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServiceConfigComponent } from './service-config/service-config.component';
 import { AuthGuardService } from '../core/auth-guard.service';
 import { RouteMonitorService } from '../core/route-monitor.service';
-import { ExemptionComponent } from './exemption.component';
+import { HomeComponent } from '../operational-system/main/home/home.component';
+import { MenuGuardService } from '../core/menu-guard.service';
 
-const routes: Routes = [{
-  path: '', component: ExemptionComponent,
-  children: [
-    { path: '', redirectTo: 'exemption', pathMatch: 'full' },
-    {
-      path: 'exemption',
-      loadChildren: () => import('./exemption-main/exemption-main.module').then(m => m.ExemptionMainModule),
-      canActivate: [AuthGuardService, RouteMonitorService]
-    },
-    { path: '**', redirectTo: 'exemption', pathMatch: 'full' }
-  ]
-}];
+const routes: Routes = [
+  {
+    path: 'service-config', component: ServiceConfigComponent,
+    canLoad: [AuthGuardService, MenuGuardService]
+  },
+  {
+    path: 'order-management',
+    loadChildren: () => import('./order-management/order-management.module').then(m => m.OrderManagementModule),
+    canLoad: [AuthGuardService, MenuGuardService]
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
