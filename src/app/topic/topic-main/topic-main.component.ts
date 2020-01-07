c09887a080d281843286affcf8746c5fa551b47f
zhouwenliang <zhouwenliang@zhumengyuan.com>
2020-01-07 09:04:21 +0800

    fix: #100710 页面路由代码优化

--- src/app/topic/topic-main/topic-main.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-topic-main',
-    templateUrl: './topic-main.component.html',
-    styleUrls: ['./topic-main.component.css']
-})
-export class TopicMainComponent {
-}
