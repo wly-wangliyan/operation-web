34848cf7b24f996dbcd37fcd4f75ac4b40f4c22e
liuqingqing <liuqingqing@zhumengyuan.com>
2020-01-09 09:24:20 +0800

    add: 添加正式版work_id

--- src/app/topic/topic-management/view-topic/view-topic.component.ts
+++ src/app/topic/topic-management/view-topic/view-topic.component.ts
@@ -10,6 +10,7 @@ import {
 import { ActivatedRoute } from '@angular/router';
 import { GlobalService } from 'src/app/core/global.service';
 import { timer } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-view-topic',
@@ -59,7 +60,15 @@ export class ViewTopicComponent implements OnInit {
   /** 获取评论列表 */
   private getCommentList() {
     this.commentSearchParams.object_id = this.topic_id;
-    this.commentSearchParams.work_id = 'e9b51d9c2dc411eab0d60242ac120006';
+
+    if (environment.version === 'd') {
+      // D版work_id
+      this.commentSearchParams.work_id = 'e9b51d9c2dc411eab0d60242ac120006';
+    } else if (environment.version === 'r') {
+      // R版work_id
+      this.commentSearchParams.work_id = '462edd5031ea11eaa7570242ac130019';
+    }
+
     this.service.requestCommentList(this.commentSearchParams).subscribe(res => {
       this.commentList = res.results;
 
