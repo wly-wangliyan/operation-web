b77120b760465fc9ffc59c321ad6e5c0e3d27852
yangsiyu <yangsiyu@zhumengyuan.com>
2020-04-15 13:39:29 +0800

    update: #105758 洗车订单>查看统计数据>列表

--- src/app/operational-system/operation/wechat/function-authorize/function-authorize-list/function-authorize-list.component.ts
+++ src/app/operational-system/operation/wechat/function-authorize/function-authorize-list/function-authorize-list.component.ts
@@ -9,38 +9,9 @@ import { PushService } from '../../push/push.service';
   styleUrls: ['./function-authorize-list.component.css']
 })
 export class FunctionAuthorizeListComponent implements OnInit {
-  public nzSearchAssistant: NzSearchAssistant;
-  constructor(
-      private globalService: GlobalService,
-      private pushService: PushService
-  ) {
-    this.nzSearchAssistant = new NzSearchAssistant(this);
-    this.nzSearchAssistant.submitSearch(true);
+
+  constructor() {
   }
 
   public ngOnInit() { }
-
-  /* NzSearchAdapter 接口实现 */
-
-  /* 请求检索 */
-  public requestSearch(): any {
-    return this.pushService.requestPushMessageList();
-  }
-
-  public continueSearch(url: string): any {
-    return this.pushService.continuePushMessageListData(url);
-  }
-
-  /* 生成并检查参数有效性 */
-  public generateAndCheckParamsValid(): boolean {
-    return true;
-  }
-
-  /* 检索失败处理 */
-  public searchErrProcess(err: any) {
-    this.globalService.httpErrorProcess(err);
-  }
-
-  /* 检索成功处理 */
-  public searchCompleteProcess(results: Array<any>, isFuzzySearch: boolean) { }
 }
--- src/app/store-maintenance/order-management/wash-car-order/statistic-data/statistic-data.component.ts
+++ src/app/store-maintenance/order-management/wash-car-order/statistic-data/statistic-data.component.ts
@@ -7,9 +7,9 @@ import {
 import { forkJoin, Subject, Subscription } from 'rxjs';
 import { GlobalConst } from '../../../../share/global-const';
 import { GlobalService } from '../../../../core/global.service';
-import { debounceTime } from 'rxjs/operators';
 import { environment } from '../../../../../environments/environment';
 import { DisabledTimeHelper } from '../../../../../utils/disabled-time-helper';
+import { NzSearchAssistant } from '../../../../share/nz-search-assistant';
 
 @Component({
   selector: 'app-statistic-data',
@@ -18,83 +18,34 @@ import { DisabledTimeHelper } from '../../../../../utils/disabled-time-helper';
 })
 export class StatisticDataComponent implements OnInit, OnDestroy {
 
-  public statisticList: Array<OrderStatisticEntity> = []; // 洗车订单
+  public nzSearchAssistant: NzSearchAssistant;
   public searchParams: StatisticSearchParams = new StatisticSearchParams(); // 洗车订单
   public order_start_time: any = ''; // 下单开始时间
-  public order_end_time: any = ''; // 下单结束时间
+  public order_end_time: any = ''; // 下单结束时间]
 
-  public pageIndex = 1; // 当前页码
-  public noResultText = '数据加载中...';
-  private requestSubscription: Subscription; // 获取数据
-  private searchText$ = new Subject<any>();
-  private continueRequestSubscription: Subscription; // 分页获取数据
-  private linkUrl: string; // 分页url
-  private searchUrl: string;
-
-  private get pageCount(): number {
-    if (this.statisticList.length % GlobalConst.NzPageSize === 0) {
-      return this.statisticList.length / GlobalConst.NzPageSize;
-    }
-    return this.statisticList.length / GlobalConst.NzPageSize + 1;
-  }
+  private searchUrl: string; // 导出url
 
   constructor(
       private globalService: GlobalService,
-      private orderService: WashOrderService) { }
-
-  public ngOnInit() {
-    this.searchText$.pipe(debounceTime(500)).subscribe(() => {
-      this.exportSearchUrl();
-      this.requestOrderList();
-    });
-    this.searchText$.next();
+      private orderService: WashOrderService
+  ) {
+    this.nzSearchAssistant = new NzSearchAssistant(this);
+    this.nzSearchAssistant.submitSearch(true);
   }
 
-  public ngOnDestroy() {
-    this.requestSubscription && this.requestSubscription.unsubscribe();
-    this.continueRequestSubscription && this.continueRequestSubscription.unsubscribe();
+  public ngOnInit() { }
+
+  public ngOnDestroy(): void {
   }
 
-  // 请求订单列表
-  private requestOrderList(): void {
-    const statisticsParams = this.searchParams.clone();
-    delete statisticsParams.page_size;
-    delete statisticsParams.page_num;
-    this.requestSubscription = this.orderService.requestOrderStatisticsData(this.searchParams)
-        .subscribe(result => {
-          this.statisticList = result.results;
-          this.linkUrl = result.linkUrl;
-          this.pageIndex = 1;
-          this.noResultText = '暂无数据';
-          this.exportSearchUrl();
-        }, err => {
-          this.pageIndex = 1;
-          this.noResultText = '暂无数据';
-          this.globalService.httpErrorProcess(err);
-        });
+  // 下单开始时间的禁用部分
+  public disabledOrderStartTime = (startValue: Date): boolean => {
+    return DisabledTimeHelper.disabledStartTime(startValue, this.order_end_time);
   }
 
-  // 翻页
-  public onNZPageIndexChange(pageIndex: number) {
-    this.pageIndex = pageIndex;
-    if (pageIndex + 1 >= this.pageCount && this.linkUrl) {
-      // 当存在linkUrl并且快到最后一页了请求数据
-      this.continueRequestSubscription && this.continueRequestSubscription.unsubscribe();
-      this.continueRequestSubscription = this.orderService.continueOrderStatisticData(this.linkUrl)
-          .subscribe(continueData => {
-            this.statisticList = this.statisticList.concat(continueData.results);
-            this.linkUrl = continueData.linkUrl;
-          }, err => {
-            this.globalService.httpErrorProcess(err);
-          });
-    }
-  }
-
-  // 条件查询
-  public onSearchBtnClick(): void {
-    if (this.generateAndCheckParamsValid()) {
-      this.searchText$.next();
-    }
+  // 下单结束时间的禁用部分
+  public disabledOrderEndTime = (endValue: Date): boolean => {
+    return DisabledTimeHelper.disabledEndTime(endValue, this.order_start_time);
   }
 
   // 导出
@@ -112,8 +63,19 @@ export class StatisticDataComponent implements OnInit, OnDestroy {
     }
   }
 
+  /* NzSearchAdapter 接口实现 */
+
+  /* 请求检索 */
+  public requestSearch(): any {
+    return this.orderService.requestOrderStatisticsData(this.searchParams);
+  }
+
+  public continueSearch(url: string): any {
+    return this.orderService.continueOrderStatisticData(url);
+  }
+
   /* 生成并检查参数有效性 */
-  private generateAndCheckParamsValid(): boolean {
+  public generateAndCheckParamsValid(): boolean {
     const sTimestamp = this.order_start_time ? (new Date(this.order_start_time).setHours(0, 0, 0, 0) / 1000).toString() : 0;
     const eTimeStamp = this.order_end_time ? (new Date(this.order_end_time).setHours(24, 0, 0, 0) / 1000).toString() : 253402185600;
     if (sTimestamp > eTimeStamp) {
@@ -128,13 +90,13 @@ export class StatisticDataComponent implements OnInit, OnDestroy {
     return true;
   }
 
-  // 下单开始时间的禁用部分
-  public disabledOrderStartTime = (startValue: Date): boolean => {
-    return DisabledTimeHelper.disabledStartTime(startValue, this.order_end_time);
+  /* 检索失败处理 */
+  public searchErrProcess(err: any) {
+    this.globalService.httpErrorProcess(err);
   }
 
-  // 下单结束时间的禁用部分
-  public disabledOrderEndTime = (endValue: Date): boolean => {
-    return DisabledTimeHelper.disabledEndTime(endValue, this.order_start_time);
+  /* 检索成功处理 */
+  public searchCompleteProcess(results: Array<any>, isFuzzySearch: boolean) {
+    this.exportSearchUrl();
   }
 }
